d6004158886236be154f062b647a739a10ef341b
michael.ackah@awaze.com <michael.ackah@awaze.com>
2023-09-04 13:11:12 +0200

    Update OPA policy

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  cloud {
-    workspaces {
-      name = "learn-terraform-drift-and-opa"
-    }
-  }
+  # cloud {
+  #   workspaces {
+  #     name = "learn-terraform-drift-and-opa"
+  #   }
+  # }
 
   required_providers {
     aws = {
